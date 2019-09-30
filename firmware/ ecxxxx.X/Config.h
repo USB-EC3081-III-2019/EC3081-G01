17cbba5a992f70784c9eae4ded3fceab85fae309
Orazio Gatto <55505804+01z4r0@users.noreply.github.com>
2019-09-30 10:05:34 -0400

    Libreria Config.h
    Esta libreria se utilizara para establecer las configuraciones iniciales de nuestr PIC16f877

---
+++ firmware/ ecxxxx.X/Config.h
@@ -0,0 +1,8 @@
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config CP = OFF         // FLASH Program Memory Code Protection bits (Code protection off)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low Voltage In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EE Memory Code Protection (Code Protection off)
+#pragma config WRT = ON          // FLASH Program Memory Write Enable (Unprotected program memory may be written to by EECON control)
