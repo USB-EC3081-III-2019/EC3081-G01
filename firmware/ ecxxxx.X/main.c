e956bcf2ef9ccdf6e719aab9184e320e548cf9a1
Orazio Gatto <55505804+01z4r0@users.noreply.github.com>
2019-09-30 10:08:58 -0400

    Actualizacion del main.c
    Se han incorporado las librerias LCD.h y Config.h para reducir la cantidad de comandos en la funcion main y esta libreria solo se utilizara para establecer todo en la funcion main

--- firmware/ ecxxxx.X/main.c
+++ firmware/ ecxxxx.X/main.c
@@ -1,38 +1,47 @@
 /*
- * File:   LCD.c
- * Author: Orazio Gatto
+ * File:   newmain.c
+ * Author: Alejandro Portillo
  *
- * Created on 24 de septiembre de 2019, 01:43 PM
+ * Created on September 27, 2019, 8:59 PM
  */
 
-#include <xc.h>
 
-#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config CP = OFF         // FLASH Program Memory Code Protection bits (Code protection off)
-#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
-#pragma config LVP = OFF        // Low Voltage In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
-#pragma config CPD = OFF        // Data EE Memory Code Protection (Code Protection off)
-#pragma config WRT = ON          // FLASH Program Memory Write Enable (Unprotected program memory may be written to by EECON control)
+#include <xc.h>
+#include "Config.h"
+#include "LCD.h" 
 
 #define _XTAL_FREQ 4000000
 
-#define LCD_RS PORTCbits.RC1
-#define LCD_E PORTCbits.RC0 
-#define LCD_DATO PORTD
-
-
-
-void LCD_Comando(char dato);
-void LCD_Escribir(char dato);
-void LCD_Configuracion_Inicial();
-void PIC_Configuracion_Inicial();
-void LCD_Escribir_Cadena(char str[]);
-
 void main ()
 {
-    char ora[] = "HOLA ¿Como estas?";
+    int Horizontal, Vertical, C;
+    
+    char aux[]="GRUPO 01";
+    char aux1[]="ORAZIO PORTILLO";
+    
+    PIC_Configuracion_Inicial();
+    LCD_Configuracion_Inicial();
+   
+    C = LCD_Contador(aux); 
+    
+    Horizontal = 4;
+    Vertical = 1; // 1 Arriba, 2 Abajo
+    
+    LCD_Cursor(Horizontal, Vertical);
+    
+    LCD_Escribir_Cadena(aux);
+    
+    ///////////////////////
+    
+    
+    Horizontal = -C-Horizontal;
+    Vertical = 2; // 1 Arriba, 2 Abajo
+    
+    LCD_Cursor(Horizontal, Vertical);
+    
+    LCD_Escribir_Cadena(aux1);
+            
+    /*char ora[] = "HOLAComo estas";
     PIC_Configuracion_Inicial();
     LCD_Configuracion_Inicial();
     
@@ -42,81 +51,15 @@ void main ()
     LCD_Escribir('a');
     LCD_Escribir('A');
     LCD_Escribir('a');
-    LCD_Escribir('A');
-    
+    LCD_Escribir('A');*/
+   
 	
-	
-    while(1){
+    while(1)
+    {
         __delay_ms(500);
         PORTAbits.RA0 = 1;
         __delay_ms(500);
         PORTAbits.RA0 = 0;
         
-    };
-}
-
-void LCD_Comando ( char dato )
-{
-    LCD_RS = 0;
-    LCD_DATO = dato;
-    LCD_E = 1;
-    LCD_E = 0;
-    
-    if(dato <= 3)
-    {
-        __delay_ms(2);
     }
-    else
-    {
-        __delay_us(40);
-    }
-    
 }
-
-void LCD_Escribir_Cadena(char str[])
-{
-    int i=0;
-    while (str[i]!= '\0'&& i<1000)
-    {
-        LCD_Escribir(str[i]);
-        i++;
-    }
-    
-}
-
-void LCD_Escribir ( char dato )
-{
-    LCD_RS = 1;
-    LCD_DATO = dato;
-    LCD_E = 1;
-    LCD_E = 0;
-    __delay_us(40);
-
-    
-}
-
-void LCD_Configuracion_Inicial ()
-{
-    LCD_E = 0;
-    __delay_ms(15);
-    LCD_Comando(0x38);
-    __delay_ms(5);
-    LCD_Comando(0x38);
-    __delay_ms(1);
-    LCD_Comando(0x38);
-    LCD_Comando(0x38);    
-    LCD_Comando(0x0C);
-    LCD_Comando(0x01);/*Display Clear*/
-    LCD_Comando(0x06);/*Incrementa el cursor*/
-    LCD_Comando(0x0C);
-}
-
-void PIC_Configuracion_Inicial ()
-{
-    TRISD = 0;
-    TRISCbits.TRISC0 = 0;
-    TRISCbits.TRISC1 = 0;
-    TRISA = 0;
-    ADCON0 = 0b00000000;
-    INTCON = 0;
-};
