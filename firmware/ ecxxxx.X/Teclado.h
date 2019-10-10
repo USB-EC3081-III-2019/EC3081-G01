941fdb2d88d597aee29f3011c140bea9f36ca114
Orazio Gatto <55505804+01z4r0@users.noreply.github.com>
2019-10-10 15:05:12 -0400

    Arreglo de teclado
    Esta vez escribe los numero uno al lado del otro y no los borra

--- firmware/ ecxxxx.X/Teclado.h
+++ firmware/ ecxxxx.X/Teclado.h
@@ -1,95 +1,138 @@
-void interrupt high_priority Teclado (void)
+void interrupt Teclado (void)
 {
-if (RBIF)
-{
-    if (PORTBbits.RB7 == 0)
+    if (RBIF)
     {
-        if ((PORTBbits.RB7==0)&&(PORTBbits.RB1==0))
+        int a=1, b=1, c=1, i=0, k=1;
+        
+        while(k==1)
         {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('#');
-        }
-        if ((PORTBbits.RB7==0)&&(PORTBbits.RB2==0))
-        {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('0');
-        }
-        if ((PORTBbits.RB7==0)&&(PORTBbits.RB3==0))
-        {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('*');
-        }
-    }
+            PORTBbits.RB3 = a;
+            PORTBbits.RB2 = b;
+            PORTBbits.RB1 = c;
+            
+            if ((PORTBbits.RB7==0)&&(PORTBbits.RB1==0))
+            {
+                LCD_Comando(0X01);
+                //__delay_ms(2);
+                //LCD_Escribir('#');
+                LCD_Escribir_Cadena("NUMERO:");
+                k=0;
+            }
+            
+            if ((PORTBbits.RB7==0)&&(PORTBbits.RB2==0))
+            {
+                //LCD_Comando(0X01);
+                __delay_ms(2);
+                LCD_Escribir('0');
+                k=0;
+            }
+            
+            if ((PORTBbits.RB7==0)&&(PORTBbits.RB3==0))
+            {
+                 //LCD_Comando(0X01);
+                __delay_ms(2);
+                LCD_Escribir('*');
+                k=0;
+            } 
     
-    if (PORTBbits.RB6 == 0)
-    {
-        if ((PORTBbits.RB6==0)&&(PORTBbits.RB1==0))
+            if ((PORTBbits.RB6==0)&&(PORTBbits.RB1==0))
+            {
+                //LCD_Comando(0X01);
+                __delay_ms(2);
+                LCD_Escribir('9');
+                k=0;
+            }
+            
+            if ((PORTBbits.RB6==0)&&(PORTBbits.RB2==0))
+            {
+                //LCD_Comando(0X01);
+                __delay_ms(2);
+                LCD_Escribir('8');
+                k=0;
+            }
+            
+            if ((PORTBbits.RB6==0)&&(PORTBbits.RB3==0))
+            {
+                //LCD_Comando(0X01);
+                __delay_ms(2);
+                    LCD_Escribir('7');
+                k=0;
+            }
+           
+            if ((PORTBbits.RB5==0)&&(PORTBbits.RB1==0))
+            {
+                //LCD_Comando(0X01);
+                __delay_ms(2);
+                LCD_Escribir('6');
+                k=0;
+            }
+            
+            if ((PORTBbits.RB5==0)&&(PORTBbits.RB2==0))
+            {
+                //LCD_Comando(0X01);
+                __delay_ms(2);
+                LCD_Escribir('5');
+                k=0;
+            }
+            
+            if ((PORTBbits.RB5==0)&&(PORTBbits.RB3==0))
+            {
+                //LCD_Comando(0X01);
+                __delay_ms(2);
+                LCD_Escribir('4');
+                k=0;
+            }  
+            
+            if ((PORTBbits.RB4==0)&&(PORTBbits.RB1==0))
+            {
+                //LCD_Comando(0X01);
+                __delay_ms(2);
+                LCD_Escribir('3');
+                k=0;
+            }
+            
+            if ((PORTBbits.RB4==0)&&(PORTBbits.RB2==0))
+            {
+                //LCD_Comando(0X01);
+                __delay_ms(2);
+                LCD_Escribir('2');
+                k=0;
+            }
+            
+            if ((PORTBbits.RB4==0)&&(PORTBbits.RB3==0))
+            {
+                //LCD_Comando(0X01);
+                __delay_ms(2);
+                LCD_Escribir('1');
+                k=0;
+            }
+               
+        if (i == 0)
         {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('9');
+            a = 0;
+            b = 1;
+            c = 1;
         }
-        if ((PORTBbits.RB6==0)&&(PORTBbits.RB2==0))
+        
+        if (i == 1)
         {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('8');
+            a = 1;
+            b = 0;
+            c = 1;
         }
-        if ((PORTBbits.RB6==0)&&(PORTBbits.RB3==0))
+        
+        if (i == 2)
         {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('7');
+            a = 1;
+            b = 1;
+            c = 0;
+            i = -1;
         }
-    }
+        i++;
     
-    if (PORTBbits.RB5 == 0)
-    {
-        if ((PORTBbits.RB5==0)&&(PORTBbits.RB1==0))
-        {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('6');
-        }
-        if ((PORTBbits.RB5==0)&&(PORTBbits.RB2==0))
-        {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('5');
-        }
-        if ((PORTBbits.RB5==0)&&(PORTBbits.RB3==0))
-        {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('4');
-        }
-    }
-    
-    if (PORTBbits.RB4 == 0)
-    {
-        if ((PORTBbits.RB4==0)&&(PORTBbits.RB1==0))
-        {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('3');
-        }
-        if ((PORTBbits.RB4==0)&&(PORTBbits.RB2==0))
-        {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('2');
-        }
-        if ((PORTBbits.RB4==0)&&(PORTBbits.RB3==0))
-        {
-            LCD_Comando(0X01);
-            __delay_ms(2);
-            LCD_Escribir('1');
-        }
-    }
+//PORTB = 0;    
+RBIF=0;
 }
-    RBIF=0;
-    
+}   
 }
+
