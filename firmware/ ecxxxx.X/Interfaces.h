47c1abf4849e798c95e38f7306c2a51e0dfcf6fe
Orazio Gatto <55505804+01z4r0@users.noreply.github.com>
2019-11-07 13:47:25 -0400

    Creaciond e libreria de interfaces

---
+++ firmware/ ecxxxx.X/Interfaces.h
@@ -0,0 +1,16 @@
+#include <xc.h>
+#include "LCD.h"
+
+#define _XTAL_FREQ 4000000
+
+
+void Bienvenida();
+void C1();
+void C2();
+void C3();
+void C4();
+void C5();
+void C6();
+void C7();
+void C8();
+void C9();
