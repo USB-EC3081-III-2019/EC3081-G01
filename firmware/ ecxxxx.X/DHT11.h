3bcc225dc7144eac010035e5f939898cf5631677
Orazio Gatto <55505804+01z4r0@users.noreply.github.com>
2019-11-29 12:37:59 -0400

    Update DHT11.h

--- firmware/ ecxxxx.X/DHT11.h
+++ firmware/ ecxxxx.X/DHT11.h
@@ -1,6 +1,9 @@
 #include <xc.h>
 
+#define _XTAL_FREQ 4000000
+
 
 void Start_Signal(void);
 __bit Check_Response();
-__bit Read_Data(unsigned char* dht_dat
+__bit Read_Data(unsigned char* dht_data);
+void DHT11();
