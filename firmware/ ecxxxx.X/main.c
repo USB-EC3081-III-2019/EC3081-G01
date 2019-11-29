39e2f223d2de0fb84382162b38f2dc56d34fa2f6
Orazio Gatto <55505804+01z4r0@users.noreply.github.com>
2019-11-29 12:44:02 -0400

    Update main.c

--- firmware/ ecxxxx.X/main.c
+++ firmware/ ecxxxx.X/main.c
@@ -12,11 +12,13 @@
 #include "Interfaz.h"
 #include "Teclado.h"
 #include "DHT11.h"
+#include "LDR.h"
+#include "PWM.h"
 
 
 #define _XTAL_FREQ 4000000
 
-unsigned int tecla, bloque, c, j, H, V, z;
+unsigned int tecla, bloque, c, j, j1, H, V, z;
 unsigned int x;
 unsigned char temp1, temp2;
 unsigned int Temp_Hum, Hum_Suelo, Lluvia, Luz;
@@ -32,13 +34,25 @@ void main() {
 
     tecla = 0;
     bloque = 0;
-    
-    while (1){
-        INTERFACE_Bienvenida();
+
+    while (1) {
+
+        LCD_Escribir_C1(3, 1, "BIENVENIDOS");
+        LCD_Escribir_C2(3, 0, "BIENVENIDOS", "Sistema de riego");
+
+        LCD_Timer();
+        
+        //OSCC1 = 0b01110011;
+        
+       // ADCON1 = 0xff;
+        
         x = 1;
-        while(x == 1){
+        
+        //myPWM ( 10 , 500 );
+        while (x == 1) {
+            
+            //myPWM ( 50 , 50 );
             INTERFACE_Principal();
         }      
     }
 }
-
