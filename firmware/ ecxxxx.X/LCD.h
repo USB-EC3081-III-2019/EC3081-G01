ce3c2f7db36c1ac59057d27614cd5eb8d7753c44
Orazio Gatto <55505804+01z4r0@users.noreply.github.com>
2019-11-29 12:39:10 -0400

    Update LCD.h

--- firmware/ ecxxxx.X/LCD.h
+++ firmware/ ecxxxx.X/LCD.h
@@ -10,7 +10,13 @@ void LCD_Comando(char dato);
 void LCD_Escribir(char dato);
 void LCD_Configuracion_Inicial();
 void PIC_Configuracion_Inicial();
+
+void LCD_Timer();
+
 void LCD_Escribir_Cadena(char str[]);
-void LCD_Cursor (int Horizontal , int Vertical);
+void LCD_Escribir_C1(char H, char V, char linea1[]);
+void LCD_Escribir_C2(char H, char H1, char linea1[] , char linea2 []);
+
+void LCD_Cursor (char Horizontal , char Vertical);
 void LCD_Display (int T, int H);
 int LCD_Contador(char c[]);
