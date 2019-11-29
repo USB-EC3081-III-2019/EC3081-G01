3046e31465c65ca595df12c4cfd1fbec5d6ed804
Orazio Gatto <55505804+01z4r0@users.noreply.github.com>
2019-11-29 12:38:29 -0400

    Update Interfaz.h

--- firmware/ ecxxxx.X/Interfaz.h
+++ firmware/ ecxxxx.X/Interfaz.h
@@ -18,51 +18,11 @@ void INTERFACE_Sistema_Automatico();
 void INTERFACE_Sistema_Automatico_Llueve();
 void INTERFACE_Sistema_Automatico_Tierra_Humeda();
 
-
 void INTERFACE_Sistema_Manual();
 void INTERFACE_Sistema_Manual_Aspersores(); 
 void INTERFACE_Sistema_Manual_Ventanas();
 
-void C1_0();
-void C1_1();
-void C1_1_1();
-void C1_1_1_1();
-void C1_1_1_2();
-void C1_1_1_3();
-
-void C1_2();
-
-void C1_1_2();
-void C1_1_2_1();
-void C1_1_2_2();
-void C1_1_2_3();
-
-void C1_1_3();
-void C1_1_3_1();
-void C1_1_3_2();
-void C1_1_3_3();
-
-void C1_1_4();
-void C1_1_4_1();
-void C1_1_4_2();
-void C1_1_4_3();
-
-void C2_0();
 void C2_1();
 void Suma();
 void Resta();
 void Neutro();
-
-void C3_0();
-void C3_1();
-void C3_2();
-void C3_3();
-void C3_4();
-void C3_5();
-
-void C4_0();
-void C4_1();
-void C4_2();
-void C4_3();
-void C4_4();
-void C4_5();
