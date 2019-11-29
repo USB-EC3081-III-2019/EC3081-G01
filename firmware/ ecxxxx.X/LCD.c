95607d7d3c559a635a14b231640db6faa8c8b337
Orazio Gatto <55505804+01z4r0@users.noreply.github.com>
2019-11-29 12:42:56 -0400

    Update LCD.c

--- firmware/ ecxxxx.X/LCD.c
+++ firmware/ ecxxxx.X/LCD.c
@@ -1,5 +1,7 @@
 #include "LCD.h"
 
+extern unsigned int c, H, V, z, tecla;
+
 void LCD_Comando(char dato) {
     LCD_RS = 0;
     LCD_DATO = dato;
@@ -14,6 +16,31 @@ void LCD_Comando(char dato) {
 
 }
 
+void LCD_Timer() {
+    for (z = 0; z < 100; z++) {
+        if (tecla == 0) {
+            __delay_ms(20);
+        }
+    }
+}
+
+void LCD_Escribir_C1(char H, char V, char linea1[]) {
+
+    LCD_Comando(0x01); //Clear Display
+    LCD_Cursor(H, V); // (Horizontal, Vertical)
+    LCD_Escribir_Cadena(linea1);
+
+
+}
+
+void LCD_Escribir_C2(char H, char H1, char linea1[], char linea2 []) {
+
+    c = LCD_Contador(linea1) + H; //Se cuenta el numero de caracteres que tiene a primera linea para que la siguiente linea quede en el lugar que se desea
+    LCD_Cursor(H1, 2); // (Horizontal, Vertical)
+    LCD_Escribir_Cadena(linea2);
+
+}
+
 void LCD_Escribir_Cadena(char str[]) {
     int i = 0;
     while (str[i] != '\0' && i < 1000) {
@@ -21,15 +48,6 @@ void LCD_Escribir_Cadena(char str[]) {
         i++;
     }
 
-}
-
-int LCD_Contador(char c[]) {
-    int i = 0;
-    while (c[i] != '\0' && i < 1000) {
-        i++;
-    }
-
-    return i;
 }
 
 void LCD_Escribir(char dato) {
@@ -58,7 +76,6 @@ void LCD_Configuracion_Inicial() {
     //2.Si se mueve o no se mueve la pantalla
     LCD_Comando(0x0C); //Display ON/OFF Cntrl
     LCD_Comando(0x0C); //1.Enciende lapantalla 2. apaga el cursor 3. no hace que titilee el cursor
-
 }
 
 void PIC_Configuracion_Inicial() {
@@ -66,29 +83,41 @@ void PIC_Configuracion_Inicial() {
     TRISD = 0;
     TRISCbits.TRISC0 = 0;
     TRISCbits.TRISC1 = 0;
-    TRISA = 0;
-    ADCON0 = 0b00000000;
-
-    TRISB = 0B11110000;
-
+    TRISCbits.TRISC6 = 0;
+    PORTCbits.RC6 = 0;
+    TRISB = 0b11110000;
+    
+    TRISAbits.TRISA5 = 0;
+    ADCON1bits.PCFG = 0b111110; //A0 será la entrada analógica
+    //Perifericos (Iniciar configuraciones analogicas)
+    ADCON0bits.ADCS = 0b00;     //ADCS1:ADCS0: A/D Conversion Clock Select bits 00 = FOSC/2    
+    ADCON0bits.ADON = 1;        //Encendemos el modulo analogico
+    ADCON1bits.ADFM = 1;        //Justificación a la derecha(De esta manera, los bits más significativos se almacenan en ADRESH)
+    //ADCON0 = 0b00000000; //Configuracion del registro ADCON0
+    /*INICIALIZACIÓN DE INTERRUPCIONES*/
+    INTCON = 0; //limpiar registro INTCON
+    
     /////////////////////////////////
 
-    OPTION_REGbits.nRBPU = 0; //Activa Pull up
-
-    //PORTBbits.RB1 = 1;// RB1 a RB3 los pone en 111
-    //PORTBbits.RB2 = 1;
-    //PORTBbits.RB3 = 1;
-
+    INTCONbits.GIE = 1; //Hab interrupciones Globales
     PORTB = 0;
-    RBIF = 0; // Bandera de Interrupciones del RB4 al RB7 a 0
-    INTCON = 0; // Limpia Registro INICON
-    INTCONbits.RBIE = 1; //Habilita Interrupciones del RB4 al RB7
-    INTCONbits.GIE = 1; // Habiita Interrupciones Gobales
+    RBIF = 0;
+    INTCONbits.RBIE = 1; //Port b
 
+    OPTION_REGbits.nRBPU = 0;
 
 }
 
-void LCD_Cursor(int h, int v) {
+int LCD_Contador(char c[]) {
+    int i = 0;
+    while (c[i] != '\0' && i < 1000) {
+        i++;
+    }
+
+    return i;
+}
+
+void LCD_Cursor(char h, char v) {
     int i;
 
     if (v == 1) {
@@ -99,7 +128,7 @@ void LCD_Cursor(int h, int v) {
     }
 
     if (v == 2) {
-        h = h + 40;
+        h = -c + h + 64 ;
         for (i = 0; i < h; i++) {
             __delay_us(40);
             LCD_Comando(0x14);
